42068790ee84b48198dc8c950da45709268cf174
Marcos J. Lopez <marcs.lopz@gmail.com>
2016-12-24 15:50:47 +0100

    ENH: calculate order or algorithm

--- src/max_pairwise_product/test_max_pairwise_product.cpp
+++ src/max_pairwise_product/test_max_pairwise_product.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <chrono>
 #include <cstdlib>
 #include <cstdint>
@@ -62,15 +63,14 @@ test_easy_case() {
   assert(result_easy_vector.first == 15 * 10);
 }
 
-void
-test_large_case() {
+uint64_t
+test_large_case(uint64_t vector_size) {
   std::vector<uint32_t> large_vector;
   random_generator<uint32_t> random_generator(0, 2e5);
   std::cout << "---------------------------" << std::endl
       << __func__ << std::endl
       << "starting generation";
   auto before = std::chrono::system_clock::now();
-  const size_t vector_size = static_cast<size_t>(1e5);
   for (std::uint64_t i = 0; i < vector_size; ++i) {
     large_vector.push_back(random_generator.generate());
     if (i % 1000 == 0)
@@ -89,6 +89,98 @@ test_large_case() {
       << "Operations: " << result_large_vector.second << std::endl
       << "Time elapsed: " << std::chrono::duration_cast<std::chrono::seconds>(after-before).count() << " seconds." << std::endl
       << std::endl;
+  return result_large_vector.second;
+}
+
+enum class order {
+  greater_than_n3,
+  n3,
+  n2logn,
+  n2,
+  nlogn,
+  n,
+  constant,
+  min = greater_than_n3,
+  max = constant
+};
+
+std::string
+calculate_order(
+    const std::pair<size_t,uint64_t> first_step,
+    const std::pair<size_t,uint64_t> second_step) {
+  const std::array<const char*, (static_cast<size_t>(order::max) - static_cast<size_t>(order::min) + 1)> order_text = {
+      "greater_than_n3",
+      "n3",
+      "n2logn",
+      "n2",
+      "nlogn",
+      "n",
+      "constant"};
+
+  const uint operations_ratio = second_step.second / first_step.second;
+
+  /// O(1)
+  const uint constant_thres = 1;
+  std::cout << "---------------------------" << std::endl
+      << __func__ << std::endl
+      << "actual ops ratio: " << operations_ratio << std::endl
+      << "constant_thres: " << constant_thres << std::endl;
+  if (operations_ratio <= constant_thres)
+    return order_text.at(static_cast<size_t>(order::constant));
+
+  /// O(n)
+  const size_t size_ratio = second_step.first / first_step.first;
+  const uint n_thres = size_ratio;
+  std::cout << "n_thres: " << n_thres << std::endl;
+  if (operations_ratio <= size_ratio)
+    return order_text.at(static_cast<size_t>(order::n));
+
+  /// O(nlogn)
+  const uint nlogn_thres =
+      size_ratio * (log(size_ratio) / log(first_step.first) + 1) + 1;
+  std::cout << "nlogn_thres: " << nlogn_thres << std::endl;
+  if (operations_ratio <= nlogn_thres)
+    return order_text.at(static_cast<size_t>(order::nlogn));
+
+  /// O(n2)
+  const uint n2_thres = size_ratio * size_ratio;
+  std::cout << "n2_thres: " << n2_thres << std::endl;
+  if (operations_ratio <= n2_thres)
+    return order_text.at(static_cast<size_t>(order::n2));
+
+  /// O(n2logn)
+  const uint n2logn_thres =
+      size_ratio * size_ratio * (log(size_ratio) / log(first_step.first) + 1) + 1;
+  std::cout << "n2logn_thres: " << n2logn_thres << std::endl;
+  if (operations_ratio <= n2logn_thres)
+    return order_text.at(static_cast<size_t>(order::n2logn));
+
+  /// O(n3)
+  const uint n3_thres = size_ratio * size_ratio * size_ratio;
+  std::cout << "n3_thres: " << n3_thres << std::endl;
+  if (operations_ratio <= n3_thres)
+    return order_text.at(static_cast<size_t>(order::n3));
+
+  /// O(greater than n3)
+  return order_text.at(static_cast<size_t>(order::greater_than_n3));
+}
+
+void
+test_large_case() {
+  std::pair<size_t,uint64_t> result_e4;
+  std::pair<size_t,uint64_t> result_e5;
+  result_e4.first = static_cast<size_t>(1e4);
+  result_e5.first = static_cast<size_t>(1e5);
+  result_e4.second = test_large_case(result_e4.first);
+  result_e5.second = test_large_case(result_e5.first);
+
+  std::cout << "---------------------------" << std::endl
+      << __func__ << std::endl
+      << "first input size: " << result_e4.first << std::endl
+      << "second input size: " << result_e5.first << std::endl
+      << "first number of ops: " << result_e4.second << std::endl
+      << "second number of ops: " << result_e5.second << std::endl
+      << "order of algorithm: " << calculate_order(result_e4, result_e5) << std::endl;
 }
 } // namespace
 
